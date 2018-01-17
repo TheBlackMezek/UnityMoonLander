f3e4f172e522d4558981d955c8c14d31c7b90a72
Alexander Thomas <s179069@students.aie.edu.au>
2018-01-17 11:25:32 -0800

    Create project with camera and lander controls

---
+++ Assets/LanderCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanderCamera : MonoBehaviour {
+
+    public float cameraRotationSpeed = 1.0f;
+    public float cameraDist = 5.0f;
+
+    public Transform lander;
+    public Camera myCamera;
+
+
+    private Vector3 camRot = Vector3.zero;
+
+
+    private void Update()
+    {
+        transform.position = lander.position;
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+
+            camRot += new Vector3(-Input.GetAxis("Mouse Y") * cameraRotationSpeed,
+                                      Input.GetAxis("Mouse X") * cameraRotationSpeed,
+                                      0);
+            
+        }
+
+        transform.eulerAngles = camRot;
+        transform.position += -transform.forward * cameraDist;
+    }
+
+}
---
+++ Assets/LanderController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanderController : MonoBehaviour {
+
+    public float gravityForce = 1.0f;
+    public float thrustrForce = 5.0f;
+    public float rotationAmt = 1.0f;
+
+    public Rigidbody body;
+    public GameObject particles;
+
+
+
+    private void FixedUpdate()
+    {
+        Vector3 rotVec = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            rotVec += new Vector3(0, -rotationAmt, 0);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotVec += new Vector3(0, rotationAmt, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rotVec += new Vector3(rotationAmt, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rotVec += new Vector3(-rotationAmt, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotVec += new Vector3(0, 0, rotationAmt);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rotVec += new Vector3(0, 0, -rotationAmt);
+        }
+
+        transform.Rotate(rotVec);
+
+
+
+        Vector3 forceVec = Vector3.down * gravityForce * Time.fixedDeltaTime;
+        if(Input.GetKey(KeyCode.Space))
+        {
+            forceVec += transform.up * thrustrForce * Time.fixedDeltaTime;
+            particles.SetActive(true);
+        }
+        else
+        {
+            particles.SetActive(false);
+        }
+
+        body.AddForce(forceVec);
+    }
+
+}
