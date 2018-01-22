2e44e16b153de6c87312934dc841581918d424af
Alexander Thomas <s179069@students.aie.edu.au>
2018-01-22 09:18:10 -0800

    Restrict camera rotation

--- Assets/LanderCamera.cs
+++ Assets/LanderCamera.cs
@@ -7,6 +7,9 @@ public class LanderCamera : MonoBehaviour {
     public float cameraRotationSpeed = 1.0f;
     public float cameraDist = 5.0f;
 
+    public float maxCamAngleX = 90.0f;
+    public float minCamAngleX = 0.0f;
+
     public Transform lander;
     public Camera myCamera;
 
@@ -20,11 +23,11 @@ public class LanderCamera : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.Mouse1))
         {
-
             camRot += new Vector3(-Input.GetAxis("Mouse Y") * cameraRotationSpeed,
-                                      Input.GetAxis("Mouse X") * cameraRotationSpeed,
-                                      0);
+                                   Input.GetAxis("Mouse X") * cameraRotationSpeed,
+                                   0);
             
+            camRot.x = Mathf.Clamp(camRot.x, minCamAngleX, maxCamAngleX);
         }
 
         transform.eulerAngles = camRot;
