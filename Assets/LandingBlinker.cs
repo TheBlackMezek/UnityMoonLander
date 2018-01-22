40240621c6e10390fd28630f18a12c18e7962549
Alexander Thomas <s179069@students.aie.edu.au>
2018-01-22 12:52:57 -0800

    Add landing pad blinkers

---
+++ Assets/LandingBlinker.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandingBlinker : MonoBehaviour {
+
+    public float blinkInterval = 1.0f;
+
+    public Material offMaterial;
+    public Material onMaterial;
+
+    public bool startOn = false;
+
+    public Renderer renderr;
+
+    private float timer = 0;
+    private bool on = false;
+
+
+
+    private void Start()
+    {
+        if (startOn)
+        {
+            renderr.material = onMaterial;
+        }
+        else
+        {
+            renderr.material = offMaterial;
+        }
+        on = startOn;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= blinkInterval)
+        {
+            timer = 0;
+            
+            if (!on)
+            {
+                renderr.material = onMaterial;
+            }
+            else
+            {
+                renderr.material = offMaterial;
+            }
+            on = !on;
+        }
+    }
+
+}
