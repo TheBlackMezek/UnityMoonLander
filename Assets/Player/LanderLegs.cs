bb845dccedcce4bc3f47225624fa39261c4e2654
Alexander Thomas <s179069@students.aie.edu.au>
2018-01-22 12:36:17 -0800

    Create level 1

---
+++ Assets/Player/LanderLegs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanderLegs : MonoBehaviour {
+
+    public LanderController lander;
+
+
+    
+
+    
+
+    private void OnTriggerStay(Collider other)
+    {
+        lander.OnLegCollision(other);
+    }
+
+}
