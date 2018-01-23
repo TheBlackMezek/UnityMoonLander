f55cf3844d4d3409bc11973853494cd5e94fdbce
Alexander Thomas <s179069@students.aie.edu.au>
2018-01-23 12:40:14 -0800

    Add starscape and rotating camera to menu

---
+++ Assets/SimpleRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour {
+
+    public Vector3 velocity;
+
+    private void Update()
+    {
+        transform.Rotate(velocity * Time.deltaTime);
+    }
+
+}
