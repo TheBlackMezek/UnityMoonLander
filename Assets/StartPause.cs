4ae95b64cc1166c3d311cb818f9cb34db36814d2
Alexander Thomas <s179069@students.aie.edu.au>
2018-01-23 15:20:32 -0800

    Add level start pause and level 2 landing pads

---
+++ Assets/StartPause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPause : MonoBehaviour {
+
+    private void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            Time.timeScale = 1;
+            Destroy(gameObject);
+        }
+	}
+
+}
